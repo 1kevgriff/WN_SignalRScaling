2fe1025e213570d341b20691dc9b9e6b17206aad
Kevin Griffin <kevin@kevgriffin.com>
2014-01-29 20:32:36 -0500

    deploy

--- SignalR in a Flash/SignalRFlash/SignalRFlash.Web/Startup.cs
+++ SignalR in a Flash/SignalRFlash/SignalRFlash.Web/Startup.cs
@@ -9,9 +9,22 @@ namespace SignalRFlash.Web
     {
         public void Configuration(IAppBuilder app)
         {
-            const string appName = "SignalRScaleoutDemo";
+            var appName = "SignalRScaleout";
 
+            //var serviceBusConnectionString = "myServiceBusConnectionString";
+            //GlobalHost.DependencyResolver.UseServiceBus(serviceBusConnectionString, appName);
 
+            //var server = "myRedisServer";
+            //var port = 6379;
+            //var password = "myPassword";
+            //GlobalHost.DependencyResolver.UseRedis(server, port, password, appName);
+
+            //SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            //builder.DataSource = "mySqlServer.com";
+            //builder.UserID = "myUserName"; // REQUIRES SCHEMA CREATION RIGHTS
+            //builder.Password = "myPassword";
+            //builder.InitialCatalog = "scaleout";
+            //GlobalHost.DependencyResolver.UseSqlServer(builder.ToString());
 
             app.MapSignalR();
         }
